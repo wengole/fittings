1f540c1d65fce4c019f64cf597f4e05b21d7801c
Col Crunch <rhartnett35@gmail.com>
2019-11-10 22:21:09 -0500

    Add models.

--- fittings/models.py
+++ fittings/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from model_utils import Choices
 
 
 # Fitting
@@ -11,7 +12,15 @@ class Fitting(models.Model):
 
 # Fitting items
 class FittingItem(models.Model):
-    flag = models.IntegerField()
+    _flag_enum = Choices('Cargo', 'DroneBay', 'FighterBay', 'HiSlot0', 'HiSlot1', 'HiSlot2',
+                         'HiSlot3', 'HiSlot4', 'HiSlot5', 'HiSlot6', 'HiSlot7', 'Invalid',
+                         'LoSlot0', 'LoSlot1', 'LoSlot2', 'LoSlot3', 'LoSlot4', 'LoSlot5',
+                         'LoSlot6', 'LoSlot7', 'MedSlot0', 'MedSlot1', 'MedSlot2', 'MedSlot3',
+                         'MedSlot4', 'MedSlot5', 'MedSlot6', 'MedSlot7', 'RigSlot0', 'RigSlot1',
+                         'RigSlot2', 'ServiceSlot0', 'ServiceSlot1', 'ServiceSlot2', 'ServiceSlot3',
+                         'ServiceSlot4', 'ServiceSlot5', 'ServiceSlot6', 'ServiceSlot7', 'SubSystemSlot0',
+                         'SubSystemSlot1', 'SubSystemSlot2', 'SubSystemSlot3')
+    flag = models.CharField(max_length=25, choices=_flag_enum, default='Invalid')
     quantity = models.IntegerField()
     type_id = models.IntegerField()
 
@@ -24,9 +33,34 @@ class Doctrine(models.Model):
     description = models.CharField(max_length=1000)
 
 
-# Dogma
-class Dogma(models.Model):
+# Dogma Attribute
+class DogmaAttribute(models.Model):
     # 12 - Low Slots | 13 - Med Slots | 14 - High Slots
+    # 1137 - Rig Slots | 1367 - Sub System Slots | 2056 - Service Slots
+    # 182 | 183 | 184 --- Req Skill 1/2/3
+    # 277 - Req. Skill 1 Lvl | 278 | 279 -- Req Skill 1/2 Lvl
     attribute_id = models.IntegerField()
-    type_id = models.IntegerField()
     value = models.DecimalField()
+
+
+# Dogma Effect
+class DogmaEffect(models.Model):
+    effect_id = models.IntegerField()
+    is_default = models.BooleanField()
+
+
+# Type Model
+class Type(models.Model):
+    name = models.CharField(max_length=500)
+    type_id = models.BigIntegerField(primary_key=True)
+    group_id = models.IntegerField()
+    published = models.BooleanField(default=False)
+    dogma_attributes = models.ManyToManyField(DogmaAttribute, related_name='types')
+    dogma_effects = models.ManyToManyField(DogmaEffect, related_name='types')
+    mass = models.FloatField(null=True)
+    capacity = models.FloatField(null=True)
+    description = models.CharField(max_length=5000, null=False)  # Not sure of the actual max.
+    volume = models.FloatField(null=True)
+    packaged_volume = models.FloatField(null=True)
+    portion_size = models.IntegerField(null=True)
+    radius = models.FloatField(null=True)
