cacafc4586848ec964ba91d3524f76e4c13bcaa8
Col Crunch <rhartnett35@gmail.com>
2019-12-13 00:05:25 -0500

    Change app name, update dependencies, and add pypi info to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@
 from setuptools import setup, find_packages
 from fittings import __version__
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 install_requires = [
     'django-bootstrap-form',
     'allianceauth>=2.0.5',
     'django-model_utils>=3.1.1',
+    'django~=2.2',
 ]
 
 testing_extras = [
@@ -13,11 +17,13 @@ testing_extras = [
 ]
 
 setup(
-    name='aa-fittings',
+    name='fittings',
     version=__version__,
     author='Col Crunch',
     author_email='it-team@serin.space',
     description='A simple fittings and doctrine management application.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=install_requires,
     extras_require={
         'testing': testing_extras,
@@ -26,7 +32,19 @@ setup(
     python_requires='~=3.4',
     license='GPLv3',
     packages=find_packages(),
-    url='https://gitlab.com/colcrunch/aa-fittings',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    url='https://gitlab.com/colcrunch/fittings',
     zip_safe=False,
     include_package_data=True,
 )
